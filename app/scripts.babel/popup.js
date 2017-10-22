1bcc14e9d3b06b1c8653311d09972f7662f4aea9
Leo <leogen17@gmail.com>
2017-10-22 14:22:55 -0400

    Only allow certain characters in time field

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,3 +1,30 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Popup');
+(function() {
+  init();
+
+  const formInputs = {
+    time: '',
+    distance: {
+      length: null,
+      type: 'miles'
+    },
+    pace: null
+  };
+
+  function init() {
+    watchForms();
+  }
+
+  function watchForms() {
+    const timeForm = document.getElementById('time-form');
+    timeForm.addEventListener('keypress', function (event) {
+      if (event.keyCode >= 48 && event.keyCode <= 57 || event.keyCode === 186 || event.keyCode === 190) {
+        formInputs.time = this.value;
+      } else {
+        event.preventDefault();
+        return false;
+      }
+    });
+  }
+})();
