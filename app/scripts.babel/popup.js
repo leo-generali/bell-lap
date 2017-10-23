ebfb27b83a22b58a31ddd6eb6012c03be2da95e8
Leo <leogen17@gmail.com>
2017-10-22 22:28:14 -0400

    Hook up all inputs to models

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,30 +1,79 @@
 'use strict';
 
 (function() {
-  init();
-
+  
   const formInputs = {
-    time: '',
+    time: 0,
     distance: {
-      length: null,
-      type: 'miles'
+      length: 0,
+      unit: 'miles'
     },
-    pace: null
+    pace: {
+      speed: 0,
+      unit: 'miles',
+      time: 'minutes'
+    }
   };
 
+  init();
+
   function init() {
-    watchForms();
+    watchTimeForm();
+    watchDistanceForm();
+    watchPaceForm();
   }
 
-  function watchForms() {
-    const timeForm = document.getElementById('time-form');
+  function watchTimeForm() {
+    const timeForm = document.getElementById('time-input');
     timeForm.addEventListener('keypress', function (event) {
-      if (event.keyCode >= 48 && event.keyCode <= 57 || event.keyCode === 186 || event.keyCode === 190) {
+      const keyCode = event.keyCode; 
+      if (keyCode >= 48 && keyCode <= 57) {
         formInputs.time = this.value;
+      } else if (keyCode === 58 || keyCode == 46) {
+        formInputs.time = this.value;        
       } else {
         event.preventDefault();
         return false;
       }
+      console.log(formInputs);
     });
   }
+  
+  function watchDistanceForm() {
+    const distanceForm = document.getElementById('distance-input');
+    distanceForm.addEventListener('keypress', function () {
+      formInputs.distance.length = this.value;
+    });
+
+    const distanceUnitForm = document.getElementById('distance-unit-select');
+    distanceUnitForm.addEventListener('change', function() {
+      formInputs.distance.unit = this.value;
+      console.log(formInputs);
+    });
+  }
+  
+  function watchPaceForm() {
+    const paceForm = document.getElementById('pace-input');
+    paceForm.addEventListener('keypress', function (event) {
+      const keyCode = event.keyCode; 
+      if (keyCode >= 48 && keyCode <= 57) {
+        formInputs.pace.speed = this.value;
+      } else if (keyCode === 58 || keyCode == 46) {
+        formInputs.pace.speed = this.value;        
+      } else {
+        event.preventDefault();
+        return false;
+      }
+      console.log(formInputs);
+    });
+
+    const paceUnitForm = document.getElementById('pace-unit-select');
+    paceUnitForm.addEventListener('change', function() {
+      formInputs.pace.unit = this.value;
+      console.log(formInputs);
+    });
+  }
+  
+  
+  
 })();
