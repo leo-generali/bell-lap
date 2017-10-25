941d244b441701d811ab216ad9b892946998fbe6
Leo <leogen17@gmail.com>
2017-10-24 23:02:52 -0400

    Calculate pace

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -72,23 +72,55 @@
     const paceUnitForm = document.getElementById('pace-unit-select');
     paceUnitForm.addEventListener('change', function() {
       formInputs.pace.unit = this.value;
-      console.log(formInputs);
     });
   }
 
   function addPaceCalculator() {
     const calculatePaceForm = document.getElementById('pace-form');
+    const paceForm = document.getElementById('pace-input');
     calculatePaceForm.addEventListener('submit', function(event) {
-      calculatePace(event, formInputs.time, formInputs.distance)
+      event.preventDefault();
+      const pace = calculatePace(formInputs.time, formInputs.distance);
+      paceForm.value = pace;
     });
   }
   
-  function calculatePace(event, time, distance) {
-    const paceMinutes = Math.floor(time / distance.length);
-    const paceSecondsNum = Math.round(((time / distance.length) - paceMinutes) * 60);
-    const paceSeconds = paceSecondsNum.toString().length === 2 ? paceSecondsNum : `0${paceSecondsNum}`;
-    return `${paceMinutes}:${paceSeconds}`;
+  function calculatePace(time, distance) {
+    const paceSeconds = returnPace(time);
+    // const paceMinutes = returnPace(time.toString());
+    // console.log(paceMinutes);
+    // const paceSecondsNum = Math.round(((time / distance.length) - time) * 6);
+    // const paceSeconds = paceSecondsNum.toString().length === 2 ? paceSecondsNum : `0${paceSecondsNum}`;
+    // console.log(paceSecondsNum, paceSeconds);
+    // return `${paceMinutes}:${paceSeconds}`;
   }
   
+  function returnPace(timeStr) {
+    const timeObject = {
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    };
+    console.log(timeStr);
+
+    const re = /[ ,]+/;
+    const timeArrHoursMins = timeStr.split(re);
+    // const timeArrHoursMins = timeStr.split(':', 2);
+
+
+    if( timeArr.length === 1 ) {
+      timeObject.minutes = parseInt(timeArr[0]); 
+    } else {
+      timeObject.hours = parseInt(timeArr[0]);
+      timeObject.minutes = parseInt(timeArr[1]); 
+    }
+    
+    console.log(timeArr);
+    console.log(timeObject);
+    // const totalSeconds = (timeObject.hours * 60) + (timeObject.minutes);
+    // console.log(totalSeconds);
+    // return totalSeconds;
+
+  }
   
 })();
